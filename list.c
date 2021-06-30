b3b7d3ee634a184c62814905e46c7a2de4c87240
Jules <jbi.octave@gmil.com>
2021-06-30 01:33:30 +0100

    Revisiting linkedlist
    Signed-off-by: Jules <jbi.octave@gmil.com>

---
+++ list.c
@@ -0,0 +1,102 @@
+#include <linux/errno.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/list.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static LIST_HEAD(todos_list);
+
+struct todos_entry {
+        struct list_head tlist; /* link in list */
+        int intvar;
+        char action[20]; /* name of a thing to do */
+};
+
+void choice(int x, struct todos_entry *xl) {
+	
+	switch(x) {
+		case 1:
+			sprintf(xl->action, "No %d Read", x);
+			return;
+		case 2: 
+			sprintf(xl->action, "No %d Code", x);
+			return;
+		case 3:
+			sprintf(xl->action, "No %d Live", x);
+			return;
+		case 4:
+			sprintf(xl->action, "No %d Sleep", x);
+			return;
+		case 5:
+			sprintf(xl->action, "No %d Exercise", x);
+			return;
+		case 6:
+			sprintf(xl->action, "No %d Connect", x);
+                        return;
+		default:
+			sprintf(xl->action, "No %d Multiply", x);
+                        return;
+	}
+}
+
+static int __init todos_init(void)
+{
+        struct todos_entry *kwanza;
+        int j;
+
+        for (j = 0; j < 10; j++) {
+                kwanza = kmalloc(sizeof(struct todos_entry), GFP_KERNEL);
+                
+		if(!kwanza) {
+			pr_info("Failed to allocate memory for ToDos list\n");
+			return -ENOMEM;
+		}
+
+		kwanza->intvar = j;
+                sprintf(kwanza->action, "ToDo %d", j + 1);
+		choice(j+1, kwanza);
+		pr_info("Adding %s at position %d to Todo list\n", kwanza->action, kwanza->intvar);
+                list_add(&kwanza->tlist, &todos_list);
+        }
+	return 0;
+}
+
+static void __exit todos_exit(void) 
+{
+	struct list_head *list; /* point to list head object */
+
+	struct list_head *tmp; /* temporary list head used for deletion */
+	
+	if (list_empty(&todos_list)) {
+                pr_info(" Todos list is empty, exiting\n");
+                return;
+        }
+        pr_info("Todos list is not empty, emptying\n");
+
+        list_for_each_safe(list, tmp, &todos_list) {
+
+                struct todos_entry *pili = list_entry(list, struct todos_entry, tlist);
+                list_del(&pili->tlist);
+                
+		pr_info("Todos (exit) : %s at position %d removed from list\n", pili->action, pili->intvar);
+                kfree(pili);
+        }
+
+        /* Checking if our list is empty */
+
+        if (list_empty(&todos_list))
+                pr_info("Done: Todos list is empty!\n");
+        else
+                pr_info("Todos list is still NOT empty though!\n");
+
+
+}
+module_init(todos_init);
+module_exit(todos_exit);
+
+MODULE_AUTHOR("Jules Irenge");
+MODULE_DESCRIPTION("linked list exercise ");
+MODULE_LICENSE("Dual BSD/GPL");
+
