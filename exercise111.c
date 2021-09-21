c82079ecc1b05ce1235b439ea8f2e58c3d7d0ecd
Jules Irenge <jbi.octave@gmail.com>
2021-09-21 01:28:43 +0100

    find a tainted modules
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>

---
+++ exercise111.c
@@ -0,0 +1,37 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int __init  module_taint(void) {
+
+struct list_head  *listx;
+struct module *modx = THIS_MODULE;
+
+listx = &modx->list;
+listx = listx->prev;
+
+pr_info("\n");
+
+list_for_each_entry(modx, listx, list) {
+
+	pr_info("%2d MOD:%20s, taints = %08lx\n", i++, modx->name, modx->taints);
+
+}
+
+return 0;
+
+}
+
+static void __exit exit_b(void) {
+
+	pr_info("Bye: unloading the module 0x%p\n", exit_b);
+
+}
+module_init(todos_init);
+module_exit(todos_exit);
+
+MODULE_AUTHOR("Jules Irenge");
+MODULE_DESCRIPTION("linked list exercise ");
+MODULE_LICENSE("Dual BSD/GPL");
+
