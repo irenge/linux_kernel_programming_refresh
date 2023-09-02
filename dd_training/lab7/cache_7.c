37e46c00c12510375a1959be0e34667ec79f2c3e
Jules Irenge <jbi.octave@gmail.com>
2023-09-02 13:05:25 +0100

    use of get_user, put_user in driver

--- dd_training/lab7/cache_7.c
+++ dd_training/lab7/cache_7.c
@@ -6,31 +6,37 @@
 #include <linux/cdev.h>		/* cdev utilities */
 
 #define MYDEV_NAME "mycdrv"
+#define MYDEV_CLASS "CustomDeviceClass"
 
 static char *ramdisk;
-#define ramdisk_size ((size_t) (16*PAGE_SIZE))
+static size_t ramdisk_size = (16 * PAGE_SIZE);
+
+static int size = 21*PAGE_SIZE;
 static struct kmem_cache *my_cache;
 static struct device *my_dev;
 
 static dev_t first;
 int major_, minor_;
 
+char *devname = "mycdrv"; 
+
 static unsigned int count = 1;
 //static int my_major = 500, my_minor;
 static struct cdev *my_cdev;
+static struct class *dev_class;
+
 
 static int mycdrv_open(struct inode *inode, struct file *file)
 {
 	static int ntimes;
-
-//	char  *kbuf = kmalloc(ramdisk_size, GFP_KERNEL);
-       ramdisk = kmem_cache_alloc(my_cache, GFP_ATOMIC);
-       if(!ramdisk) {
-	       pr_err("\nfailed to create a cache object\n");
-	       return -ENOMEM;
+	
+	ramdisk = kmem_cache_alloc(my_cache, GFP_ATOMIC);
+	
+	if(!ramdisk) {
+               pr_err("\nfailed to create a cache object\n");
+               return -ENOMEM;
        }
        dev_info(my_dev, "a cache object successfully created\n");
-       //	file->private_data = kbuf;
 
 	(ntimes == 0) ? pr_info("\nDevice %s opened 1st time ", MYDEV_NAME) : pr_info("\nDevice %s opened %d times ", MYDEV_NAME, ntimes + 1);
 	ntimes += 1;
@@ -45,11 +51,8 @@ static int mycdrv_open(struct inode *inode, struct file *file)
 static int mycdrv_release(struct inode *inode, struct file *file)
 {
 	kmem_cache_free(my_cache, ramdisk);
-	//char  *cbuf = file->private_data;
-	dev_info(my_dev, "destroyed a memory cache object\n");
 	pr_info(" CLOSING device: %s:\n\n", MYDEV_NAME);
 	return 0;
-	//kfree(cbuf);
 }
 
 	static ssize_t
@@ -58,6 +61,11 @@ mycdrv_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos)
 	int nbytes;
 
 	char  *kbuf = kmalloc(ramdisk_size, GFP_KERNEL);
+	 if(!kbuf) {
+                pr_err("Failed to allocated memory at read\n");
+                return 0;
+        }
+
 
 	file->private_data = kbuf;
 
@@ -81,6 +89,10 @@ mycdrv_write(struct file *file, const char __user *buf, size_t lbuf,
 	int nbytes;
 
 	char  *kbuf = kmalloc(ramdisk_size, GFP_KERNEL);
+	if(!kbuf) {
+		pr_err("Failed to allocated memory at write\n");
+		return 0;
+	}
 
 	file->private_data = kbuf;
 
@@ -106,28 +118,80 @@ static const struct file_operations mycdrv_fops = {
 
 static int __init my_init(void)
 {
-	//ramdisk = kmalloc(ramdisk_size, GFP_KERNEL);
-	//	first = MKDEV(my_major, my_minor);
-	//	register_chrdev_region(first, count, MYDEV_NAME);
-	int ret;
-	ret = alloc_chrdev_region ( &first, 0, count, MYDEV_NAME);
+	 my_cache = kmem_cache_create("mycache", size, 0,
+                                     SLAB_HWCACHE_ALIGN, NULL);
+        if (!my_cache) {
+                pr_err("kmem_cache_create failed\n");
+                return -ENOMEM;
+        }
+
+	// dynamic
+	if ((alloc_chrdev_region ( &first, 0, count, devname)) < 0) {
+		pr_err("failed to allocate character device region\n");
+		(void)kmem_cache_destroy(my_cache);
+		return -EBUSY;
+	}
 	pr_info("Using dynamic alloc...\n");
 	major_ = MAJOR(first);
 	minor_ = MINOR(first);
-	pr_info("%s device allocated, dev [%i, %i]\n", MYDEV_NAME, major_, minor_);
+	pr_info("%s device allocated, dev [%i, %i]\n", devname, major_, minor_);
 
 	my_cdev = cdev_alloc();
+	if(!my_cdev) {
+		pr_err("cdev_alloc failed !");
+		unregister_chrdev_region(first, count);
+		(void)kmem_cache_destroy(my_cache);
+		return -EBUSY;
+	}
+	pr_info("\ndevice registered\n");
+	
+	// Optional udev thing
+	
+	/* Create Device class : using udev */
+	if ((dev_class = class_create(MYDEV_CLASS)) == NULL) {
+                pr_info("Device class can not be created !\n");
+                goto ClassError;
+        }
+
+	/* Create a device file */
+        if (device_create(dev_class, NULL, first, NULL,"%s", devname) == NULL) {
+                pr_info("Can not create device file\n");
+                goto FileError;
+	}
+	//
+
+	/* Initialise device */
 	cdev_init(my_cdev, &mycdrv_fops);
-	cdev_add(my_cdev, first, count);
-	pr_info("\nReturn = %d ==> Succeeded in registering character device %s\n",ret, MYDEV_NAME);
+	
+	if(cdev_add(my_cdev, first, count) < 0) {
+		pr_err("cdev_add() failed\n");
+		goto AddError;
+		//cdev_del(my_cdev); /***** investigate *** 
+               // unregister_chrdev_region(first, count);
+	}
+	pr_info("\nDevice is live\n");
+
+	//pr_info("\nReturn = %d ==> Succeeded in registering character device %s\n",ret, MYDEV_NAME);
 	return 0;
+
+AddError:
+        device_destroy(dev_class, first);
+FileError:
+        class_destroy(dev_class);
+ClassError:
+        unregister_chrdev_region(first, count);
+	(void)kmem_cache_destroy(my_cache);
+        return -1;
 }
 
 static void __exit my_exit(void)
 {
 	cdev_del(my_cdev);
+	device_destroy(dev_class, first);
+        class_destroy(dev_class);
 	unregister_chrdev_region(first, count);
 	pr_info("\ndevice unregistered\n");
+	(void)kmem_cache_destroy(my_cache);
 }
 
 module_init(my_init);
