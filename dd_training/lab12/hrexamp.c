343dd6d37e63fd3420317ad00cdb8e6593feee0e
Jules Irenge <jbi.octave@gmail.com>
2023-09-30 20:38:50 +0100

    hrtimer example

---
+++ dd_training/lab12/hrexamp.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/timer.h>
+#include <linux/init.h>
+#include <linux/version.h>
+#include <linux/ktime.h>
+#include <linux/hrtimer.h>
+
+static struct kt_data {
+	struct hrtimer timer;
+	ktime_t period;
+} *data;
+
+static  enum hrtimer_restart ktfun(struct hrtimer *var)
+{
+	struct  my_data *data = container_of(var, struct kt_data, timer);
+	ktime_t now = var->base->get_timer();
+
+	pr_info("timer running at jiffies=%d\n", jiffies);
+	hrtimer_forward(var, now, data->period);
+	return HRTIMER_RESTART;
+}
+static int __init my_init(void)
+{
+	data = kmalloc(sizeof(*data), GFP_KERNEL);
+	data->period = ktime_set(1, 0);
+	hrtimer_init(&data->timer, CLOCK_REALTIME, HRTIMER_MODE_REL);
+	data->timer.function = ktfun;
+	hrtimer_start(&data->timer, data->period, HRTIMER_MODE_REL);
+
+	return 0;
+}
+static void __exit my_exit(void)
+{
+	hrtimer_cancel(&data->timer);
+	kfree(data);
+}
+module_init(my_init);
+module_exit(my_exit);
+MODULE_LICENSE("GPL v2");
+
