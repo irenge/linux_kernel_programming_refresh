fa62bc09eef27fee125e18f74536fe37b3b26f60
Jules Irenge <jbi.octave@gmail.com>
2023-10-16 23:30:24 +0100

    hrtimer with periodic time

--- dd_training/lab12/hrtimer_example.c
+++ dd_training/lab12/hrtimer_example.c
@@ -11,7 +11,6 @@ static struct kt_data {
 	struct hrtimer timer;
 	int period_in_secs;
 	unsigned long start_time;
-	char struct_id;
 	ktime_t period;
 } *data;
 
@@ -20,17 +19,26 @@ static  enum hrtimer_restart ktfun(struct hrtimer *var)
 	struct  kt_data *data = container_of(var, struct kt_data, timer);
 	ktime_t now = var->base->get_time();
 
-	pr_info("timer running at jiffies=%ld\n", jiffies);
+	pr_info(" Kernel hrtimer: period = %d elapsed = %ld\n", data->period, jiffies - data->start_time);
+	pr_info("\nCurrent pid = %d\n", (int) current->pid);
+
+
 	hrtimer_forward(var, now, data->period);
 	return HRTIMER_RESTART;
 }
 static int __init my_init(void)
 {
+	pr_info("\nCurrent pid = %d\n", (int) current->pid);
 	data = kmalloc(sizeof(*data), GFP_KERNEL);
 	data->period = ktime_set(1, 0);
+	data->start_time = jiffies; // Not sure about this 
+
 	hrtimer_init(&data->timer, CLOCK_REALTIME, HRTIMER_MODE_REL);
 	data->timer.function = ktfun;
 	hrtimer_start(&data->timer, data->period, HRTIMER_MODE_REL);
+
+	pr_info("\nLaunching timer with period of %ld seconds\n",
+			data->period);
 
 	return 0;
 }
