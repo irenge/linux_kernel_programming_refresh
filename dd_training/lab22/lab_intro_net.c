2f80794cdfc0a147060fc35e31b87220b59c754e
Jules Irenge <jbi.octave@gmail.com>
2023-11-08 22:10:54 +0000

    Add an example of receive

--- dd_training/lab22/lab_intro_net.c
+++ dd_training/lab22/lab_intro_net.c
@@ -2,7 +2,7 @@
 #include<linux/netdevice.h>
 #include <linux/init.h>
 #include <linux/version.h>
-
+#include <linux/etherdevice.h>
 
 struct net_device *my_net;
 struct my_priv *priv;
@@ -43,7 +43,17 @@ static void printline(struct net_device *dev, unsigned char *data, int n)
 }
 
 
-static int my_start_xmit(struct sk_buff *skb, struct net_device *dev) {
+static void net_receive(struct sk_buff *skb, struct net_device *dev) {
+//	static int pkts;
+
+	netdev_info(dev, "\n packets received \n");
+	stats->rx_bytes += skb->len;
+	++stats->rx_packets;
+	netif_rx(skb);
+}
+
+
+static int net_transmit(struct sk_buff *skb, struct net_device *dev) {
 	int len; 
 	char *data;
 	int i;
@@ -65,41 +75,46 @@ static int my_start_xmit(struct sk_buff *skb, struct net_device *dev) {
 
 	netdev_info(dev, "\n");
 	/* so we can free it in interrupt routine */
-	//priv->skb = skb;
+	//priv->skb = skb; /* remove to allow the loop back net_receive
 
 	//mynet_hw_tx(data, len, dev);
-	dev_kfree_skb(skb);
+	//dev_kfree_skb(skb);
 
 	stats->tx_bytes += skb->len;
 	++stats->tx_packets;
 
+	/* loop back */
+	net_receive(skb, dev);
+
 	return 0;
 }
+
 /*
    static void mynet_rx(struct net_device *dev, int len, unsigned char *buf) {
    struct 
    */
 
 static struct net_device_stats *mynet_stats(struct net_device *dev) {
-	return &dev->stats;
+	 netdev_info(dev, "Hit: %s(%s)\n", __func__, dev->name);
+	 return &dev->stats;
 }
 
 
 static struct net_device_ops ndo = {
 	.ndo_open = ndo_open,
 	.ndo_stop = ndo_stop,
-	.ndo_start_xmit = my_start_xmit,
+	.ndo_start_xmit = net_transmit,
 	.ndo_get_stats = mynet_stats,
 };
 
 void get_mac(struct net_device *dev, char *mac, int len) {
 
 	unsigned int i;
-        int j;
-	
+	int j;
+
 	netdev_info(dev, "\nMac address length is %d\n", len);
 
-	/* Fill in the MAC address with random number */
+	// Fill in the MAC address with random number 
 	for (j = 0; j < len; ++j) {
 		get_random_bytes(&i, sizeof(int)); //fill the entire bitspace of the int instead of only one byte
 		i = i % 15;
@@ -107,7 +122,7 @@ void get_mac(struct net_device *dev, char *mac, int len) {
 		netdev_info(dev, "\ni = %d\n", i);
 		mac[j] = (char)i;
 	}
-	
+
 	dev_addr_mod(dev, 0, mac, len);
 
 }
@@ -124,12 +139,21 @@ static void mynet_setup(struct net_device *dev)
 
 	//dev_addr_mod(dev, 0, phony, ETH_ALEN);
 
+	//eth_hw_addr_random(dev);
+
 	ether_setup(dev);
 	dev->netdev_ops = &ndo;
 	dev->flags |= IFF_NOARP;
+
+	//	eth_hw_addr_random(dev);
+
+	//dev->min_mtu = 10;
+	//dev->max_mtu = 30;
+
+
 	stats = &dev->stats;
 
-	stats->collisions = 10;
+	stats->collisions = 50;
 
 }
 
@@ -137,20 +161,33 @@ static int __init net_load(void){
 
 	/* Device registration */
 
+	int ret;
+
 	my_net = alloc_netdev(0, "mynet%d", NET_NAME_ENUM, mynet_setup);
+
 	if(!my_net)
 		return -ENOMEM;
-	if(register_netdev(my_net))
-		pr_info("Error in registering device %s\n", my_net->name);
+
+	ret = register_netdev(my_net);
+
+	if(ret) {
+		pr_err("Error in registering device %s\n", my_net->name);
+		free_netdev(my_net);
+		return ret;
+	}
+
 	netdev_info(my_net, "Succeeded in loading %s!\n\n", dev_name(&my_net->dev));
+
 	return 0;
 
 }
 
 static void __exit net_unload(void) {
-	netdev_info(my_net, "Unloading stub network module\n\n");
+	
+	netdev_info(my_net, "<0> Cleaning Up the network module\n");
 	unregister_netdev(my_net);
 	free_netdev(my_net);
+
 
 }
 
@@ -158,7 +195,5 @@ module_init(net_load);
 module_exit(net_unload);
 
 MODULE_AUTHOR("Jules Irenge");
-MODULE_AUTHOR("Jerry Cooperstein");
-MODULE_AUTHOR("Tatsuo Kawasaki");
-MODULE_DESCRIPTION("Intro network");
+MODULE_DESCRIPTION("Simple Network driver");
 MODULE_LICENSE("GPL v2");
