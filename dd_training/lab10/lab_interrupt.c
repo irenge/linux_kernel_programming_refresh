bea39a04cb2aaceda2155ffa9a6df6a0f19a3de7
Jules Irenge <jbi.octave@gmail.com>
2024-04-24 15:39:45 +0100

    Update

--- dd_training/lab10/lab_interrupt.c
+++ dd_training/lab10/lab_interrupt.c
@@ -1,18 +1,28 @@
 #include <linux/interrupt.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/time.h>
 
-
-#define SHARED_IRQ 174
+#define SHARED_IRQ 156
 
 static unsigned int irq = SHARED_IRQ, mydev_id, irq_counter;
 int ret;
 
+// interrupt handler
 static irqreturn_t my_interrupt(int irq, void *dev_id) {
+	struct timeval tv;
+	struct tm broken;
+	int written;
+	do_gettimeofday(&tv);
+
+
+	time64_to_tm(tv.tv_sec, 0, &broken);
+	pr_info("%d:%d:%d:%ld\n", broken.tm_hour, broken.tm_min, broken.tm_sec, tv.tv_usec);
 	irq_counter++;
 	pr_info("In the ISR: counter = %d\n", irq_counter);
 	return  IRQ_NONE;
 }
+
 
 static int __init my_interrupt_init(void) {
 	if((ret = request_irq(irq, my_interrupt, IRQF_SHARED, "hello_interrupt", &mydev_id))){
